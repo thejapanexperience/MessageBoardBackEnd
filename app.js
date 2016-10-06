20eba7b0ce26dbf16d5bf6ec6c9d2a1eb1306f35
Richard Mands <richard@eduku.org>
2016-10-06 09:40:05 -0700

    working BackEnd

--- app.js
+++ app.js
@@ -1,155 +1,184 @@
 const PORT = 8000
 
-const http = require('http')
-// const qs = require('querystring')
-const md5 = require('md5')
-const moment = require('moment')
+const http = require ('http')
+const uuid = require ('uuid')
+const moment = require ('moment')
+const body = require ('body')
+const fs = require ('fs')
+const anyBody = require('body/any');
 
-// GET /foods ---> retrieve all foods
-// POST /foods$food=banana ---> create a new food
 
-// 1. Set up maths /math/add/40/16  DONE
-// 2. Gravatar URL Generator DONE
-// Example: GET /gravatar/c@codinghouse.co would respond with the string
-// "http://www.gravatar.com/avatar/c417050fa67e3d2e9b690636c2ce80ec
-// 3. Sentence info DONE
-// 4. Time since DOB  DONE
+const filename = 'data.json'
 
-let answer
 const server = http.createServer((req, res) => {
-    // req === request obj received
-    // res === response obj - methods for responding
+  let { url, method } = req
 
-    let {url, method} = req
+  console.log(`${method} ${url}`);
+  console.log('Hello');
 
-    console.log(`${method} ${url}`)
+  let [notSure, path, messageNumber, two, three] = url.split('/');
+  console.log(path, messageNumber, two, three);
 
-    console.log('Hello');
+  let messages
+  let newMessageBody
 
-    let [notSure,
-        path,
-        one,
-        two,
-        three,
-        four,
-        five] = url.split('/')
+  function _readMessages(callback) {
+   fs.readFile(filename, (err, buffer) => {
+    messages = JSON.parse(buffer)
+    console.log('messages', messages);
+    callback()
+    })
+  }
 
-    console.log('notSure', notSure, 'path', path, 'one', one, 'two', two, 'three', three)
+  function _writeMessages(callback) {
+    anyBody(req, (err, body) => {
+      newMessageBody = body
+      console.log('body:', newMessageBody);
+      fs.readFile(filename, (err, buffer) => {
+        messages = JSON.parse(buffer)
+        console.log('messages', messages);
+        console.log('in POST',messages, newMessageBody)
+        newMessageBody.timestamp = moment().format('LLLL');
+        console.log(newMessageBody);
+        newMessageBody.id = uuid();
+        console.log(newMessageBody);
+        messages.push( newMessageBody );
+        fs.writeFile(filename, JSON.stringify(messages), err => {
+            console.log('done!');
+          })
+      callback()
+    });
+  })}
 
-    let emailMD5 = md5(one)
-    console.log(emailMD5)
+  function _editMessages(callback) {
+    anyBody(req, (err, body) => {
+      newMessageBody = body
+      console.log('body:', newMessageBody);
+      fs.readFile(filename, (err, buffer) => {
+        messages = JSON.parse(buffer)
+        console.log('messages', messages);
+        if (messageNumber > messages.length || messageNumber < 1) {
+          res.statusCode = 404
+          res.end('Not Found')
+        } else {
+          console.log('in PUT',messages, newMessageBody)
+          messages[messageNumber-1].message = newMessageBody.message
+          messages[messageNumber-1].author = newMessageBody.author
+          messages[messageNumber-1].timestamp = moment().format('LLLL');
+          fs.writeFile(filename, JSON.stringify(messages), err => {
+            console.log('done!');
+          })
+      callback()
+    }
+  })})}
 
-    let string = decodeURI(one)
-    console.log('string', string);
+  function _deleteMessages(callback) {
+    anyBody(req, (err, body) => {
+      newMessageBody = body
+      console.log('body:', newMessageBody);
+      fs.readFile(filename, (err, buffer) => {
+        messages = JSON.parse(buffer)
+        console.log('messages', messages);
+        if (messageNumber > messages.length || messageNumber < 1) {
+          res.statusCode = 404
+          res.end('Not Found')
+        } else {
+          console.log('in DELTE',messages, newMessageBody)
+          messages.splice(messageNumber-1, 1)
+          fs.writeFile(filename, JSON.stringify(messages), err => {
+            console.log('done!');
+          })
+      callback()
+    }
+  })})}
 
-    let charCount = string.length
-    console.log('charCount', charCount);
+  switch (path) {
 
-    let arr = string.split(' ')
-    console.log('arr',arr)
+    case 'messages':
 
-    let wordCount = arr.length
-    console.log('wordCount',wordCount);
+      switch(method) {
 
-    let aveWordLength = Math.floor(charCount / wordCount)
-    console.log('aveWordLength', aveWordLength);
+        case 'GET':
 
-    let year = one
-    let month = two
-    let day = three
-    let DOB = year + month + day
-    let age = moment(DOB, "YYYYMMDD").fromNow();
-    console.log('You were born', age);
+          switch (messageNumber) {
 
-    switch (path) {
-        case 'math':
+            case undefined:
+            case "":
+              _readMessages(() => {
+                res.end(JSON.stringify(messages))
+              })
+              break;
 
-            switch (method) {
-                case 'GET':
+            case `${messageNumber}` :
+            _readMessages(() => {
+              console.log(messageNumber);
+              let index = parseInt(messageNumber) - 1
+              console.log(messages);
+              res.end(JSON.stringify(messages[index]))
+            })
+              break;
 
-                    switch (one) {
-                        case 'add':
-                            answer = parseInt(two) + parseInt(three);
-                            res.end(JSON.stringify(answer));
-                            break;
-                        case 'subtract':
-                            answer = parseInt(two) - parseInt(three);
-                            res.end(JSON.stringify(answer));
-                            break;
-                        case 'multiply':
-                            answer = parseInt(two) * parseInt(three);
-                            res.end(JSON.stringify(answer));
-                            break;
-                        case 'divide':
-                            answer = parseInt(two) - parseInt(three);
-                            res.end(JSON.stringify(answer));
-                            break;
-                        case 'exponent':
-                            answer = Math.pow(parseInt(two), parseInt(three));
-                            res.end(JSON.stringify(answer));
-                            break;
-                        default:
-                            res.statusCode = 404
-                            res.end('Not Found')
+            default:
+              res.statusCode = 404
+              res.end('Not Found')
+              break
+          }
+          break;
 
-                    }
-                    break
-                default:
-                    res.statusCode = 404
-                    res.end('Not Found')
+        case 'POST':
+          _writeMessages(() => {
 
-            }
-            break
+              res.end(JSON.stringify(messages))
+            })
+          break;
 
-        case 'gravatar':
+        case 'PUT':
 
-            switch (method) {
-                case 'GET':
-                  console.log(`http://www.gravatar.com/avatar/${emailMD5}`);
-                  res.end(`http://www.gravatar.com/avatar/${emailMD5}`);
-                  break;
+          switch (messageNumber) {
 
-                default:
-                    res.statusCode = 404
-                    res.end('Not Found')
+            case `${messageNumber}` :
 
-            }
-            break
+            _editMessages(() => {
+                res.end(JSON.stringify(messages[messageNumber-1]))
+              })
+            break;
 
-        case 'sentence':
+            default:
+              res.statusCode = 404
+              res.end('Not Found')
+          }
+          break
 
-            switch (method) {
-                case 'GET':
-                  console.log(`Word Count: ${wordCount}, Character Count: ${charCount}, Average Word Length: ${aveWordLength}.`);
-                  res.end(`Sentence info - Word Count: ${wordCount}, Character Count: ${charCount}, Average Word Length: ${aveWordLength}.`);
-                  break;
 
-                default:
-                    res.statusCode = 404
-                    res.end('Not Found')
+        case 'DELETE':
+        switch (messageNumber) {
 
-            }
-            break
+          case `${messageNumber}` :
 
-        case 'age':
+          _deleteMessages(() => {
+              res.end(JSON.stringify(messages))
+            })
 
-            switch (method) {
-                case 'GET':
-                  res.end(`You were born ${age}`);
-                  break;
+          break;
 
-                default:
-                    res.statusCode = 404
-                    res.end('Not Found')
-
-            }
-            break
-        default:
+          default:
             res.statusCode = 404
             res.end('Not Found')
-    }
+        }
+        break
+
+        default:
+          res.statusCode = 404
+          res.end('Not Found')
+      }
+      break
+    default:
+      res.statusCode = 404
+      res.end('Not Found')
+  }
 })
 
+
 server.listen(PORT, err => {
-    console.log(err || `Server listening on port ${PORT}`)
+  console.log(err || `Server listening on port ${PORT}`);
 })
